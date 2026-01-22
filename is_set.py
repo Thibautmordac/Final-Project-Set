92bc929d824c2ebd2124780262fdf0354529bfeb
Jim737li <jim737li@gmail.com>
2026-01-22 14:07:20 +0100

    is_set.py
    Volgende bestand geeft een functie die checkt of 3 gegeven kaarten een 'set' vormen.

---
+++ is_set.py
@@ -0,0 +1,24 @@
+#alle verschillende eigenschappen
+kleuren = ['red', 'purple', 'green']
+symbolen = ['oval', 'squiggle', 'diamond']
+schaduw = ['empty', 'filled', 'shaded']
+afb_kaarten = {}
+
+#kent 4 eigenschappen toe aan 1 kaart
+class Kaart:
+    def __init__(self, color, symbol, shading, number):
+        self.color = color
+        self.symbol = symbol
+        self.shading = shading
+        self.number = number
+    
+    def __repr__(self):
+        k = f'Kaart({self.color}, {self.symbol}, {self.shading}, {self.number})'
+        return k
+
+#checkt om te zien dat 3 gegeven kaarten een set vormen
+def is_set(k1, k2, k3):
+    return ((k1.number+k2.number+k3.number % 3) == 0 and
+            (k1.symbol+k2.symbol+k3.symbol % 3) == 0 and
+            (k1.shading+k2.shading+k3.shading) % 3 == 0 and
+            (k1.color+k2.color+k3.color) % 3 == 0)
