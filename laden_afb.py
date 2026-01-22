94e39faa5cf87018e5c088f9dafa2da5738e1829
Jim737li <jim737li@gmail.com>
2026-01-22 14:39:46 +0100

    laden_afb.py

---
+++ laden_afb.py
@@ -0,0 +1,17 @@
+from is_set import *
+import os
+import pygame
+
+def laden_afb():
+    global afb_kaarten
+    map = 'kaarten'
+    for color in kleuren:
+        for symbol in symbolen:
+            for shading in schaduw:
+                for number in [1,2,3]:
+                    bestandsnaam = f'{color}{symbol}{shading}{number}.gif'
+                    pad = os.path.join(map, bestandsnaam)
+                    if os.path.isfile(pad):
+                        afb = pygame.image.load(pad).convert_alpha()
+                        key = (kleuren.index(color), symbolen.index(symbol), schaduw.index(shading), number)
+                        afb_kaarten[key] = afb
