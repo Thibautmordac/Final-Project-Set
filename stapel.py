0c9afe38f84b825184f69ea2d2cad41148f3d8f4
Jim737li <jim737li@gmail.com>
2026-01-28 12:24:06 +0100

    Update stapel.py

--- stapel.py
+++ stapel.py
@@ -1,12 +1,14 @@
-from is_set import Kaart
+import pygame
+from variabelen import *
+from is_set import *
 
-#Genereert een stapel van 81 kaarten met enkel getallen
 def creëren_stapel():
+    """Genereert een stapel van 81 kaarten met enkel getallen"""
     stapel = [Kaart(k, sy, sc, g) for k in range(3) for sy in range(3) for sc in range(3) for g in range(3)]
     return stapel
 
-#Trekt kaart uit de stapel
-def kaart_trekken(kaart, x, y, index, gekozen=False):
+def kaart_trekken(kaart, x, y, index, gekozen=False, scherm=None):
+    """Trekt kaart uit de stapel"""
     bg = (180,220,255) if gekozen else (240,240,240)
     pygame.draw.rect(scherm, bg, (x, y, kaartbreedte, kaartlengte))
     key = (kaart.color, kaart.symbol, kaart.shading, kaart.number + 1)
