e4402053d3f4ad12e234356feb060856a1fcc929
Jim737li <jim737li@gmail.com>
2026-01-28 12:23:10 +0100

    Update beurt_speler.py

--- beurt_speler.py
+++ beurt_speler.py
@@ -1,16 +1,17 @@
 from time import time
 import sys
 import pygame
+from is_set import *
 from tafel import *
 from variabelen import *
 
-def beurt_speler(tafel, stapel, score, bericht):
+def beurt_speler(tafel, stapel, score, bericht, scherm, kleine_letter, grote_letter):
     """Zorgt ervoor dat knoppen werken, er een tijdslimiet is
     , scores worden bijgewerkt en stapel wordt aangepast"""
     geklikt = []
     start_tijd = time()
     while time() - start_tijd < 30:
-        toon_tafel(tafel, geklikt, bericht, score)
+        toon_tafel(tafel, geklikt, bericht, score, scherm, kleine_letter, grote_letter)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -28,13 +29,15 @@ def beurt_speler(tafel, stapel, score, bericht):
                     if len(geklikt) == 3:
                         if is_set(tafel[geklikt[0]], tafel[geklikt[1]], tafel[geklikt[2]]):
                             score['Speler'] += 1
-                            for p in sorted(geklikt, reversed =True):
+                            for p in sorted(geklikt, reverse=True):
                                 if stapel:
                                     tafel[p] = stapel.pop(0)
                                 else:
                                     del tafel[p]
                             while len(tafel) < 12 and stapel:
                                 tafel.append(stapel.pop(0))
+                            geklikt.clear()
+                            return True
                         else:
                             geklikt.clear()
                             bericht = 'Is geen SET'
